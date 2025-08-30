67390299d001804a00f15ada96d747b4076e3208
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-08-30 11:16:11 +0200

    add spectre dependency

--- JCode/Program.cs
+++ JCode/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Spectre.Console;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+var content = Emoji.Replace("[underline green]Hello[/] World! :rocket:");
+var ui = new Panel(content) { Border = BoxBorder.Rounded };
+AnsiConsole.Write(ui);
